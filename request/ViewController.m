1ab87c881de875f2b94e0c6c929b769aee5c45a2
saringkhan_ <saringkhan_h@hotmail.com>
2014-12-22 16:57:14 +0700

    fit
    fit

--- request/ViewController.m
+++ request/ViewController.m
@@ -24,4 +24,23 @@
     // Dispose of any resources that can be recreated.
 }
 
+- (IBAction)sync:(id)sender {
+    
+    NSURLRequest *request = [[NSURLRequest alloc] initWithURL:[NSURL URLWithString:@"http://202.44.47.48/info_graphic/test.php"]];
+    NSURLConnection *theConnection=[[NSURLConnection alloc] initWithRequest:request delegate:self];
+    
+    if (theConnection) {
+        UIAlertView *alert = [[UIAlertView alloc] initWithTitle:@"Sync"
+                                                        message:@"Synchronous Request URL"
+                                                       delegate:self
+                                              cancelButtonTitle:@"OK"
+                                              otherButtonTitles:nil];
+        [alert show];
+
+        NSData *theData = [NSURLConnection sendSynchronousRequest:request returningResponse:nil error:nil];
+        NSDictionary *newJSON = [NSJSONSerialization JSONObjectWithData:theData options:0 error:nil];
+        NSLog(@"JSON: %@", newJSON);
+    }
+}
+
 @end
